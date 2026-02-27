04943e72fabfca172f8bd8c57b215305a3949e80
Geovana <geovanamscardoso183@gmail.com>
2026-02-27 09:54:29 -0300

    Implementar métodos estáticos de JOptionPane

--- src/br/com/fiap/main/ClienteTest.java
+++ src/br/com/fiap/main/ClienteTest.java
@@ -4,12 +4,26 @@ import br.com.fiap.entities.Cliente;
 import javax.swing.*;
 
 public class ClienteTest {
+
+    static String texto(String j){
+        return  JOptionPane.showInputDialog(j);
+    }
+
+    static int inteiro(String j){
+        return  Integer.parseInt(JOptionPane.showInputDialog(j));
+    }
+
+    static double real(String j){
+        return  Double.parseDouble(JOptionPane.showInputDialog(j));
+    }
+
+
     public static void main(String[] args){
        Cliente objCliente = new Cliente();
-       objCliente.setNome(JOptionPane.showInputDialog("Informe o nome do cliente: "));
-       objCliente.setCpf(JOptionPane.showInputDialog("Informe o CPF do cliente: "));
-       objCliente.setIdade(Integer.parseInt(JOptionPane.showInputDialog("Informe a idade do cliente: ")));
-       objCliente.setAltura(Double.parseDouble(JOptionPane.showInputDialog("Informe a altura do cliente: ")));
+       objCliente.setNome(texto("Informe o nome do cliente: "));
+       objCliente.setCpf(texto("Informe o CPF do cliente: "));
+       objCliente.setIdade(inteiro("Informe a idade do cliente: "));
+       objCliente.setAltura(real("Informe a altura do cliente: "));
 
         System.out.println(objCliente);
     }
